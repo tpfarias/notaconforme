ad019900c163987f68b33f94bf62b254fa902415
Tarcisio Farias <tarcisioparaiso@gmail.com>
2024-12-30 22:19:26 -0300

    Finalização retorno array com códigos dos serviços preditos

--- api/v1/endpoints/predicao.py
+++ api/v1/endpoints/predicao.py
@@ -9,12 +9,12 @@ from sqlalchemy.future import select
 from core.deps import get_session, get_current_user
 from schemas.predicao_schema import PredicaoSchema
 
-#vectorizer_path = "MLModels/vectorizer_PNL.pkl"
-vectorizer_path = "MLModels/vectorizer_ficticio.pkl"
+#vectorizer_path = "MLModels/vectorizer_multilabel_randomForest.pkl"
+vectorizer_path = "MLModels/vectorizer_multilabel_randomForest_simulado.pkl"
 vectorizer = joblib.load(vectorizer_path)
 
-#modelo_path = "MLModels/model_ML.joblib"
-modelo_path = "MLModels/modelo_ficticio.joblib"
+#modelo_path = "MLModels/modelo_multilabel_randomForest.joblib"
+modelo_path = "MLModels/modelo_multilabel_randomForest_simulado.joblib"
 modelo_treinado = joblib.load(modelo_path)
 
 router = APIRouter()
@@ -24,13 +24,23 @@ async def post_predicoes(request: List[PredicaoSchema], usuario_logado: UsuarioM
 
     response = []
 
+    classes = [
+        '0401', '0402', '0403', '0404', '0405', '0406', '0407', '0408',
+        '0409', '0410', '0411', '0412', '0413', '0414', '0415', '0416',
+        '0417', '0418', '0419', '0420', '0421', '1'
+    ]
+
     for item in request:
         try:
             discriminacao_vetorizada = vectorizer.transform([item.discriminacao.lower()])
             cod_servico = modelo_treinado.predict(discriminacao_vetorizada)
-            response_data = {"servicos": str(cod_servico)}
-            response_data.update(item.model_dump(exclude={"discriminacao"}))
+            codigos_selecionados = [classes[i] for i in range(len(classes)) if cod_servico[0, i] == 1]
 
+            response_data = {
+                "servicos": codigos_selecionados  # Lista de códigos selecionados
+            }
+
+            response_data.update(item.model_dump(exclude={"discriminacao"}))
             response.append(response_data)
 
 
