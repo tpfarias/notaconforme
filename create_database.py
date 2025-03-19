d4a0f6e2d439d0d83317ae6dc135fbbcbd8d9753
Tarcisio Farias <tarcisioparaiso@gmail.com>
2025-03-19 10:30:09 -0300

    Inclusão Usuario Admin como padrão na base de dados

--- create_database.py
+++ create_database.py
@@ -1,5 +1,8 @@
 from core.config import settings
-from core.database import engine
+from core.database import engine, SessionLocal
+from models.usuario_model import UsuarioModel  # Importe o modelo de usuário
+from core.security import gerar_hash_senha
+from passlib.context import CryptContext
 
 
 async def create_tables() -> None:
@@ -10,6 +13,28 @@ async def create_tables() -> None:
         await conn.run_sync(settings.DBBaseModel.metadata.drop_all)
         await conn.run_sync(settings.DBBaseModel.metadata.create_all)
     print('Tabelas criadas com sucesso...')
+
+    # Criar usuário padrão
+    await create_default_user()
+
+
+async def create_default_user():
+    db = SessionLocal()
+    try:
+        # Verificar se o usuário já existe
+        if not db.query(UsuarioModel).filter_by(email="admin@email.com").first():
+            hashed_password = gerar_hash_senha("admin123")  # Senha segura
+            user = UsuarioModel(nome="Admin", email="admin@email.com", senha=hashed_password, ativo=True)
+            db.add(user)
+            db.commit()
+            print("Usuário padrão criado com sucesso.")
+        else:
+            print("Usuário padrão já existe.")
+    except Exception as e:
+        print(f"Erro ao criar usuário padrão: {e}")
+        db.rollback()
+    finally:
+        db.close()
 
 
 if __name__ == '__main__':
