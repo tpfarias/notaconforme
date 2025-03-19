50e01bbfff5bfb529e7cae3ecdb2848eebd25672
Tarcisio Farias <tarcisioparaiso@gmail.com>
2025-03-19 12:10:36 -0300

    Inclusão Usuario Admin como padrão na base de dados 3

--- core/security.py
+++ core/security.py
@@ -1,9 +1,10 @@
 from passlib.context import CryptContext
 
-CRIPTO = CryptContext(schemes=['bcrypt'], deprecated='auto')
+CRIPTO = CryptContext(schemes=['bcrypt'], deprecated='auto', bcrypt__default_rounds=12)
 
 def verificar_senha(senha: str, hash_senha: str) -> bool:
      return CRIPTO.verify(senha, hash_senha)
 
 def gerar_hash_senha(senha: str) -> str:
     return CRIPTO.hash(senha)
+
